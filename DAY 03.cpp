1f72df3755f4dfb8470c24e1724fc9e70591fe43
sumit-kharat-05 <sumitkharat686@gmail.com>
2026-06-07 23:57:15 +0530

    DSA Day 03

---
+++ DAY 03.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//Conditional statements//
+
+// int n = -2;
+
+// if(n > 0)
+// {
+//     cout << "Positive Number" << endl;
+// }
+// else
+// {
+//     cout << "Negative number" << endl;
+// }
+
+
+// int age;
+// cout << "Enter Your Age : ";
+// cin >> age;
+
+// if(age >= 18)
+// {
+//     cout << "You can Vote" << endl;
+// }
+// else
+// {
+//     cout << "You Cannot Vote" << endl;
+// }
+
+
+// int n;
+// cout << "Entered A Number :";
+// cin >> n;
+
+// if(n%2 == 0)
+// {
+//     cout << "Even Number" << endl;
+// }
+// else
+// {
+//     cout << "Odd Number" << endl;           
+// }
+
+// int grade;
+// cout << "Entered Your Grade :";
+// cin >> grade;
+
+// if(grade >=90)
+// {
+//     cout << "A" <<endl;
+// }
+// else if(grade >=80)
+// {
+//     cout << "B" <<endl;
+// }
+// else if(grade >=70)
+// {
+//     cout << "C" <<endl;
+// }
+// else if(grade >=60)
+// {
+//     cout << "D" <<endl;
+// }
+// else
+// {
+// cout << "Fail" <<endl; 
+// }
+
+char ch;
+cout<< "Entered Character :";
+cin >> ch;
+
+if(ch >= 'a' && ch <= 'z')
+{
+    cout <<"Lowercase" << endl;
+}
+else
+{
+    cout << "Uppercase" << endl;
+}
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
