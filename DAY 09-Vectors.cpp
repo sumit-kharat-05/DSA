ed4f7b6c9aa0f55295c826ac047f6c3153059841
sumit-kharat-05 <sumitkharat686@gmail.com>
2026-07-22 23:41:25 +0530

    Vectors

--- DAY 09-Vectors.cpp
+++ DAY 09-Vectors.cpp
@@ -24,9 +24,9 @@
 //     cout << vec[2] << endl;
 //     cout << vec[3] << endl;
 //     cout << vec[4] << endl;
-   
+
 //     vector<int>vect(vec);
-    
+
 //     for(int x:vect)
 //     {
 //         cout<<x<<" ";
@@ -36,21 +36,45 @@
 //     return 0;
 // }
 
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-    
-    vector<char> vec = {'S','U','M','I','T'};
-    
-    cout<<"Size"<<vec.size()<<endl;
-    for(char val : vec)
+
+    vector<char> vec = {
+        'S',
+        'U',
+        'M',
+        'I',
+        'T',
+    };
+
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('K');
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('H');
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('A');
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('R');
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('A');
+    cout << "Size =" << vec.size() << endl;
+    vec.push_back('T');
+    cout << "Size =" << vec.size() << endl;
+    vec.pop_back();
+    cout << "Size =" << vec.size() << endl;
+    cout << "Capacity =" << vec.capacity() << endl;
+     cout << "Empty =" << vec.empty() << endl;
+     cout<< "Front Element ="<<vec.front()<<endl;
+     cout<< "Back Element ="<<vec.back()<<endl;
+     cout<< "Index Element ="<<vec.at(4)<<endl;
+    for (char val : vec)
     {
-        cout<< val <<endl;
+        cout << val << endl;
     }
-   
+
     return 0;
 }
